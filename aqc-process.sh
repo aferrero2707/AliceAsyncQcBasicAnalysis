f82ff7c123d98f8dc1f186257003b2d873d0d235
aferrero2707 <aferrero1975@gmail.com>
2025-05-22 09:34:20 +0200

    Add automatic update of runlists by default in aqc-process.sh
    The automatic update of runlists and fetching of the corresponding root
    files can be disabled by passing the -s option, like this:
    
        ./aqc-process.sh -s

--- aqc-process.sh
+++ aqc-process.sh
@@ -4,8 +4,19 @@ export INFOLOGGER_MODE=stdout
 export SCRIPTDIR=$(readlink -f $(dirname $0))
 #echo "SCRIPTDIR: ${SCRIPTDIR}"
 
+SKIP_UPDATE=0
+if [ x"$1" = "x-s" ]; then
+    SKIP_UPDATE=1
+    shift
+fi
+
 RUNS_CONFIG="$1"
 PLOTS_CONFIG="$2"
+
+if [ x"${SKIP_UPDATE}" = "x0" ]; then
+    ./aqc-get-completed-runs.sh -u "${RUNS_CONFIG}"
+    ./aqc-fetch.sh "${RUNS_CONFIG}"
+fi
 
 YEAR=$(jq ".year" "${RUNS_CONFIG}" | tr -d "\"")
 PERIOD=$(jq ".period" "${RUNS_CONFIG}" | tr -d "\"")
